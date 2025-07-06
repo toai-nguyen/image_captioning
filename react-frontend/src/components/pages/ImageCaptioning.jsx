7a036b9aaaf4803af49bdea966b0a738c6b19e29
toai-nguyen <maxngudan@gmail.com>
2025-07-06 14:43:05 +0700

    add tailwind

--- react-frontend/src/components/pages/ImageCaptioning.jsx
+++ react-frontend/src/components/pages/ImageCaptioning.jsx
@@ -1,7 +1,8 @@
 export default function ImageCaptioning() {
     return (
         <div>
-            <h1>Image Captioning Page</h1>
+            <h1 className="text-2xl font-bold mb-4" style={{ textAlign: 'center' }}
+            >Image Captioning Page</h1>
             <p>This page is dedicated to image captioning functionalities.</p>
         </div>
     );
